fc1ffa1541de130e0461c5c8f8d2ff29c4a772cb
Aurorainic <aurorainic86@yahoo.com>
2025-05-17 21:48:22 +0800

    fix: node ssr error (1)

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,12 +1,13 @@
+// .vitepress/theme/Layout.vue
 <script setup>
 import DefaultTheme from 'vitepress/theme'
-import { useWaline } from 'vitepress-theme-website'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  import('@waline/client/dist/waline.css') // 动态引入样式，避免 SSR 报错
+})
 </script>
 
 <template>
-  <DefaultTheme.Layout>
-    <template #doc-after>
-      <Waline />
-    </template>
-  </DefaultTheme.Layout>
+  <DefaultTheme.Layout />
 </template>
