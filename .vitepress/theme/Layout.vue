289f93d21d15687bc74146e4f4773ebe5984ed0c
Aurorainic <aurorainic86@yahoo.com>
2025-05-17 21:29:25 +0800

    docs: 引入Waline评论+小幅修改

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import { useWaline } from 'vitepress-theme-website'
+</script>
+
+<template>
+  <DefaultTheme.Layout>
+    <template #doc-after>
+      <Waline />
+    </template>
+  </DefaultTheme.Layout>
+</template>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,10 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
+import WalineComment from './components/WalineComment.vue';
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import { useWaline } from 'vitepress-theme-website'
+import { useData } from 'vitepress';
 import './style.css'
 
 export default {
@@ -13,5 +16,11 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
     // ...
+  },
+  setup() {
+    useWaline({
+      serverURL: 'https://fediwiki-comment.tkg3.top',  // 替换为你的 Waline 后端地址
+      dark: 'auto',
+    })
   }
 } satisfies Theme
