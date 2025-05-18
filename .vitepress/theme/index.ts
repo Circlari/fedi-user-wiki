2869492d528d285f19db752a2b5c27ba850f3689
Aurorainic <aurorainic86@yahoo.com>
2025-05-18 10:10:54 +0800

    fix: 移除waline

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,11 +2,7 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import { useWaline } from 'vitepress-theme-website'
-import { useData } from 'vitepress';
 import './style.css'
-import WalineComment from './components/WalineComment.vue';
-
 
 export default {
   extends: DefaultTheme,
@@ -19,9 +15,6 @@ export default {
     // ...
   },
   setup() {
-    useWaline({
-      serverURL: 'https://fediwiki-comment.tkg3.top',  // 替换为你的 Waline 后端地址
-      dark: 'auto',
-    })
+
   }
 } satisfies Theme
